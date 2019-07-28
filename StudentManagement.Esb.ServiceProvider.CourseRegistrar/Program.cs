cfe5aced45c20b5317ec94f7a073c733b1e3db0b
Thomas N. Eaves <eavestn@gmail.com>
2019-07-28 19:46:31 -0400

    Correct project structure

--- StudentManagement.Esb.ServiceProvider.CourseRegistrar/Program.cs
+++ StudentManagement.Esb.ServiceProvider.CourseRegistrar/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shuttle.Core.ServiceHost;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace StudentManagement.Esb.ServiceProvider.CourseRegistrar
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ServiceHost.Run<ServiceProvider>();
         }
     }
 }
---
+++ StudentManagement.Esb.ServiceProvider.CourseRegistrar/ServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using Shuttle.Core.ServiceHost;
+using Shuttle.Esb;
+
+namespace StudentManagement.Esb.ServiceProvider.CourseRegistrar
+{
+    public class ServiceProvider : IServiceHostStart
+    {
+        public IServiceBus _serviceBus;
+
+        public void Start()
+        {
+
+        }
+        
+        public void Stop()
+        {
+            this._serviceBus.Dispose();
+        }
+    }
+}
